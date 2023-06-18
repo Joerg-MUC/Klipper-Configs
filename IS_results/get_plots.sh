b55164e0f52140e460b5b0f942cfc157e356aa9c
Joerg <joerg4@gitlab.com>
2023-06-18 08:03:12 +0200

    Initial Commit for deepblue Printer is up and running

---
+++ IS_results/get_plots.sh
@@ -0,0 +1,2 @@
+~/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o ~/printer_data/config/IS_results/shaper_calibrate_x.png
+~/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o ~/printer_data/config/IS_results/shaper_calibrate_y.png
